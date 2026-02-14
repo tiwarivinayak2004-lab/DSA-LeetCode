2e3c7453d4809d7f9ac7481b8e3c9b5efdaec206
Vinayak Tiwari <tiwarivinayak2004@gmail.com>
2026-02-15 02:00:35 +0530

    Time: 0 ms (100.00%) | Memory: 49.7 MB (12.31%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -17,6 +17,31 @@ class Solution {
     public int countNodes(TreeNode root) {
         if(root==null) return 0;
 
+        int left=leftsubtreeHeight(root);
+        int right=rightsubtreeHeight(root);
+
+        if(left==right) return (1<<left)-1;
+
         return 1+countNodes(root.left)+countNodes(root.right);
+    }
+    public int leftsubtreeHeight(TreeNode node)
+    {
+        int height=0;
+        while(node!=null)
+        {
+            height++;
+            node=node.left;
+        }
+        return height;
+    }
+    public int rightsubtreeHeight(TreeNode node)
+    {
+        int height=0;
+        while(node!=null)
+        {
+            height++;
+            node=node.right;
+        }
+        return height;
     }
 }
