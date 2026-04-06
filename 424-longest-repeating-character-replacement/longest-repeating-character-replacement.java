8083738de0a59d85a005eccc5b708af909795400
Vinayak Tiwari <tiwarivinayak2004@gmail.com>
2026-04-06 16:48:52 +0530

    Time: 13 ms (38.64%) | Memory: 45.9 MB (92.50%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -3,17 +3,17 @@ class Solution {
         int l=0,r=0,maxlen=0;
         int hash[]=new int[26];
         int maxf=0;
-        while(r<s.length()){
+        while(r<s.length()){      
             char right=s.charAt(r);
             hash[right-'A']++;
             maxf=Math.max(maxf,hash[right-'A']);
-            while((r-l+1)-maxf>k){
+            if((r-l+1)-maxf>k){                   //while can also be used here
                 char left =s.charAt(l);
                 hash[left -'A']--;
                 maxf=0;
-                for(int i=0;i<26;i++){
-                    maxf=Math.max(maxf,hash[i]);
-                }
+                // for(int i=0;i<26;i++){
+                //     maxf=Math.max(maxf,hash[i]);
+                // }
                 l=l+1;
             }
             if((r-l+1)-maxf<=k) maxlen=Math.max(maxlen,r-l+1);
