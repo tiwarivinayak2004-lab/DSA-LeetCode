f1f14fcaa1086ab67c68fa2930351396ef25c89b
Vinayak Tiwari <tiwarivinayak2004@gmail.com>
2026-04-21 13:15:21 +0530

    Time: 2 ms (99.61%) | Memory: 44.6 MB (60.96%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+
+        int freq[]=new int[26];
+        for(char c:s.toCharArray())
+        {
+            freq[c-'a']++;
+        }
+        for(char c:t.toCharArray())
+        {
+            freq[c-'a']--;
+            if(freq[c-'a']<0) return false;
+        }
+        return true;
+    }
+}
