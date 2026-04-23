08abb7005a62c8072060c4472d8e5963396492ef
Vinayak Tiwari <tiwarivinayak2004@gmail.com>
2026-04-23 20:12:25 +0530

    Time: 16 ms (93.42%) | Memory: 45.5 MB (69.02%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -1,21 +1,62 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-        int closest=nums[0]+nums[1]+nums[2];
-        for(int i=0;i<nums.length-2;i++){
-            int left=i+1;
-            int right=nums.length-1;
-            while(left<right){
-                int sum=nums[i]+nums[left]+nums[right];
+        int close=nums[0]+nums[1]+nums[2];
+        int n=nums.length;
+        for(int i=0;i<n-2;i++){
+            int l=i+1;
+            int r=n-1;
+            while(l<r){
+            int sum=nums[i]+nums[l]+nums[r];
 
-                if(Math.abs(sum-target)<Math.abs(closest-target))
-                closest=sum;
+            if(Math.abs(sum-target)<Math.abs(close-target)){
+                close=sum;
+            }
 
-                if(sum<target) left++;
-                else if(sum>target) right--;
-                else return sum;
+            if(sum<target) l++;
+            else if(sum>target) r--;
+            else return sum;
             }
         }
-        return closest;
+        return close;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Arrays.sort(nums);
+//         int closest=nums[0]+nums[1]+nums[2];
+//         for(int i=0;i<nums.length-2;i++){
+//             int left=i+1;
+//             int right=nums.length-1;
+//             while(left<right){
+//                 int sum=nums[i]+nums[left]+nums[right];
+
+//                 if(Math.abs(sum-target)<Math.abs(closest-target))
+//                 closest=sum;
+
+//                 if(sum<target) left++;
+//                 else if(sum>target) right--;
+//                 else return sum;
+//             }
+//         }
+//         return closest;
