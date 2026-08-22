f28696003cc824a55fab86d81688413e52483650
Vinayak Tiwari <tiwarivinayak2004@gmail.com>
2026-08-22 21:51:45 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (19.09%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int sum;
+        int max=0;
+        for(int i=0;i<accounts.length;i++){
+            sum=0;
+            for(int j=0;j<accounts[0].length;j++){
+                sum+=accounts[i][j];
+            }
+            max=Math.max(max,sum);
+        }
+        return max;
+    }
+}
