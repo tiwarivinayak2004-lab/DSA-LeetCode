63c49e9ea35d46db620959f3816241d8f78cce0b
Vinayak Tiwari <tiwarivinayak2004@gmail.com>
2026-09-03 19:39:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (52.55%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -3,12 +3,17 @@ class Solution {
         if(n==0) return 0;
         if(n==1) return 1;
 
-        int[] dp=new int[n+1];
-        dp[0]=0;
-        dp[1]=1;
+        //int[] dp=new int[n+1];
+        //dp[0]=0;
+        //dp[1]=1;
+        int prev=0;
+        int prev1=1;
+        int curr=0;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            curr=prev+prev1;
+            prev=prev1;
+            prev1=curr;
         }
-        return dp[n];
+        return curr;
     }
 }
