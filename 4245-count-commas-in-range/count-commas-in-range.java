a19c24cef6df53f10ffc43048eaa9f24793ccc5c
Vinayak Tiwari <tiwarivinayak2004@gmail.com>
2026-09-08 18:21:48 +0530

    Time: 1 ms (83.65%) | Memory: 42.5 MB (72.85%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -1,6 +1,12 @@
 class Solution {
     public int countCommas(int n) {
-        if(n<999) return 0;
-        else return n-999;
+        int total=0;
+        if(n>999){
+            total=(n-1000)+1;
+        }
+        else{
+            total=0;
+        }
+        return total;
     }
 }
