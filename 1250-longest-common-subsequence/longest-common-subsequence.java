2da3c6a4e0fd32ecf4b361d894217931ab78759b
Vinayak Tiwari <tiwarivinayak2004@gmail.com>
2026-09-08 18:11:42 +0530

    Time: 19 ms (91.71%) | Memory: 54.2 MB (10.41%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -3,7 +3,6 @@ class Solution {
         int l1=text1.length();
         int l2=text2.length();
         int[][] dp=new int[l1+1][l2+1];
-        dp[0][0]=0;
         for(int i=1;i<=l1;i++){
             for(int j=1;j<=l2;j++){
                 if(text1.charAt(i-1)==text2.charAt(j-1)){
