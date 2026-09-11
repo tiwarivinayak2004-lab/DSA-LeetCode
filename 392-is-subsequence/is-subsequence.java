01d09a4c2bbd9e46ad3e862343b31541c94725f3
Vinayak Tiwari <tiwarivinayak2004@gmail.com>
2026-09-11 23:03:59 +0530

    Time: 5 ms (1.89%) | Memory: 43.3 MB (5.84%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -2,20 +2,20 @@ class Solution {
     public boolean isSubsequence(String s, String t) {
         int l1=s.length();
         int l2=t.length();
-        int dp[][]=new int[l2+1][l1+1];
-        for(int i=1;i<=l2;i++){
-            for(int j=1;j<=l1;j++){
-                if(t.charAt(i-1)==s.charAt(j-1)){
-                    dp[i][j]=dp[i-1][j-1]+1;
+        boolean dp[][]=new boolean[l1+1][l2+1];
+        for(int j=0;j<=l2;j++){
+            dp[0][j]=true;
+        }
+        for(int i=1;i<=l1;i++){
+            for(int j=1;j<=l2;j++){
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
                 }
                 else{
-                    int skip1=dp[i-1][j];
-                    int skip2=dp[i][j-1];
-                    dp[i][j]=Math.max(skip1,skip2);
+                    dp[i][j]=dp[i][j-1];
                 }
             }
         }
-        if(dp[l2][l1]==l1) return true;
-        else return false;
+        return dp[l1][l2];
     }
 }
