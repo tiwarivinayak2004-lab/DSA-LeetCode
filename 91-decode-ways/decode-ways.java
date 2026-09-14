74669b0cf8c87b8def51767d9a9599c4914b1bf1
Vinayak Tiwari <tiwarivinayak2004@gmail.com>
2026-09-14 22:17:30 +0530

    Time: 1 ms (81.51%) | Memory: 43.3 MB (21.98%) - LeetSync

---
+++ 91-decode-ways/decode-ways.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n=s.length();
+        int dp[]=new int[n+1];
+        if(s.charAt(0)=='0') return 0;
+        dp[0]=1;
+        if(n==1) return 1;
+        // if(s.charAt(1)!='0'){
+        //     dp[1]=dp[0];
+        // }
+        // int num=Integer.parseInt(s.substring(0,2));
+        // if(num>=10 && num<=26){
+        //     dp[1]+=1;
+        // }
+        for(int i=1;i<=n;i++){
+            if(s.charAt(i-1)!='0'){
+                dp[i]=dp[i]+dp[i-1];
+            }
+            if(i>=2){
+                int num1=Integer.parseInt(s.substring(i-2,i));
+                if(num1 >= 10 && num1 <= 26){
+                    dp[i]=dp[i]+dp[i-2];
+                }
+            }
+        }
+        return dp[n];
+    }
+}
