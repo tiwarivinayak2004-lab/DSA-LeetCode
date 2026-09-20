4bcdd39717ab3400fb66e93e43a6b4caf52953f2
Vinayak Tiwari <tiwarivinayak2004@gmail.com>
2026-09-20 14:04:37 +0530

    Time: 4 ms (36.53%) | Memory: 43 MB (92.92%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char ch1=s.charAt(i);
+            if(ch1=='(' || ch1=='{' || ch1=='[')
+            {
+                st.push(ch1);
+            }
+            else
+            {
+                if(st.isEmpty()) return false;
+                char ch=st.peek();
+                st.pop();
+                if(ch=='(' && ch1!=')' || ch=='{' && ch1!='}' || ch=='[' && ch1!=']')
+                {
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
