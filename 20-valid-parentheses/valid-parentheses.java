4de21e9e1ef37f73971d0d00dedad2c5b6027c0f
Vinayak Tiwari <tiwarivinayak2004@gmail.com>
2026-09-20 14:05:35 +0530

    Time: 3 ms (85.92%) | Memory: 43.2 MB (72.74%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,9 +1,8 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> st=new Stack<>();
-        for(int i=0;i<s.length();i++)
+        for(char ch1: s.toCharArray())
         {
-            char ch1=s.charAt(i);
             if(ch1=='(' || ch1=='{' || ch1=='[')
             {
                 st.push(ch1);
